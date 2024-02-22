aec7e86fbd5090b8fe9d6bcdf8561e6eee85688c
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2024-02-22 16:30:44 +0100

    Modify permission path

---
+++ cd2023bfs2g5-model/src/main/resources/db/migration/V4__permission_path_.sql
@@ -0,0 +1,16 @@
+-- Auto-generated SQL script #202402221627
+UPDATE tserver_permission
+	SET permission_name='com.campusdual.cd2023bfs2g5.api.core.service.ICategoryService/categoryQuery'
+	WHERE id_server_permission=1;
+UPDATE tserver_permission
+	SET permission_name='com.campusdual.cd2023bfs2g5.api.core.service.ICategoryService/categoryInsert'
+	WHERE id_server_permission=2;
+UPDATE tserver_permission
+	SET permission_name='com.campusdual.cd2023bfs2g5.api.core.service.ICategoryService/categoryUpdate'
+	WHERE id_server_permission=3;
+UPDATE tserver_permission
+	SET permission_name='com.campusdual.cd2023bfs2g5.api.core.service.ICategoryService/categoryDelete'
+	WHERE id_server_permission=4;
+UPDATE tserver_permission
+	SET permission_name='com.campusdual.cd2023bfs2g5.api.core.service.ICategoryService/categoryActiveQuery'
+	WHERE id_server_permission=5;
